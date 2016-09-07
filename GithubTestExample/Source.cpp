46da6610e745146b3dd7539798d10fbfce145fb5
jmhong <jeongmo.hong@gmail.com>
2016-09-07 15:54:53 +0900

    Commit test.

--- GithubTestExample/Source.cpp
+++ GithubTestExample/Source.cpp
@@ -1,4 +1,41 @@
+#include <stdio.h>
+#include <iostream>
+#include <ostream>
+#include <fstream>
+
+//namespace
+struct Vector2D
+{
+	int x = 1;
+	int y = 2;
+
+	void print()
+	{
+		printf("%d %d\n", x, y);
+	}
+};
+
+std::ostream& operator << 
+	(std::ostream& stream, Vector2D vector)
+{
+	stream << vector.x << " " << vector.y << std::endl;
+
+	return stream;
+}
+
 void main()
 {
-	// my first github code.
+	Vector2D vector;
+/*
+	FILE *of = fopen("text.txt", "w");
+
+	fprintf(of, "%3.1f %d", vector.x, vector.y);
+
+	fclose(of);
+
+	std::cout << vector << std::endl;
+*/
+	std::ofstream of("cppstyle.txt");
+
+	of << vector << std::endl;
 }
